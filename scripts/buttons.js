377999129de2dd6b212e8b0b25559b06085d2374
CholiShawn <71330406+CholiShawn@users.noreply.github.com>
2020-10-19 15:12:04 -0400

    I added a second question to the 'about me' page

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -5,7 +5,13 @@ function greeting() {
 function farewell() {
     alert("Bye bye, see you soon!");
 }
+
 function question(){
-    let colour = prompt("What's your favourite colour?");
-    alert("Ah yes, I also like " + colour) 
+    let name = prompt("Hii! What's your name?");
+    alert(name + "? What a lovely name! Nice to meet you, " + name + "!");
+}
+
+function question2() {
+    let colour = prompt("Hi Again! What's your favourite colour?");
+    alert("Ah yes, the colour " + colour + ". A fine one indeed"); 
 }
