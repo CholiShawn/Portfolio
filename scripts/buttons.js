fb8d8fa20a4032f7e058941fa198402780a365eb
CholiShawn <71330406+CholiShawn@users.noreply.github.com>
2020-10-19 14:42:30 -0400

    Imade a scripts file and added buttons

---
+++ scripts/buttons.js
@@ -0,0 +1,11 @@
+function greeting() {
+    alert("Hello and Welcome :D");
+}
+
+function farewell() {
+    alert("Bye bye, see you soon!");
+}
+function question(){
+    let colour = prompt("What's your favourite colour?");
+    alert("Ah yes, I also like " + colour) 
+}
