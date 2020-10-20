79d9796b66ac4304cc5c65baf30c82de3f30afee
CholiShawn <71330406+CholiShawn@users.noreply.github.com>
2020-10-20 08:54:04 -0400

    I added two more buttons in the assignments and individual projects section

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -3,7 +3,7 @@ function greeting() {
 }
 
 function farewell() {
-    alert("Bye bye, see you soon!");
+    alert("Bye bye, see you soon (hopefully)!");
 }
 
 function question(){
@@ -14,4 +14,12 @@ function question(){
 function question2() {
     let colour = prompt("Hi Again! What's your favourite colour?");
     alert("Ah yes, the colour " + colour + ". A fine one indeed"); 
+}
+function question3() {
+    let animal = prompt("Just one more question, what's your favourite animal?")
+    alert(animal + " are great :)")
+}
+function question4() {
+    let subject = prompt("Ok I lied, this is the last question: What's your favoruite subject in school?")
+    alert(subject + "?" + (" Sounds like fun!"))
 }
