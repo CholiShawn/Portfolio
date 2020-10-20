ff692cc70217e93200f82f101c42b9f7d576175c
CholiShawn <71330406+CholiShawn@users.noreply.github.com>
2020-10-20 09:27:45 -0400

    added a comment in about.css

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -15,10 +15,12 @@ function question2() {
     let colour = prompt("Hi Again! What's your favourite colour?");
     alert("Ah yes, the colour " + colour + ". A fine one indeed"); 
 }
+
 function question3() {
     let animal = prompt("Just one more question, what's your favourite animal?");
     alert(animal + " are great :)");
 }
+
 function question4() {
     let subject = prompt("Ok I lied, this is the last question: What's your favoruite subject in school?");
     alert(subject + "?" + " Sounds like fun!");
