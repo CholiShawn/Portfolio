f00536032c46fe57e8058a6e63fb8de3120e1922
CholiShawn <71330406+CholiShawn@users.noreply.github.com>
2020-11-03 15:53:08 -0500

    changed a lot of buttons cause larger number function wasn't working

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,5 +1,5 @@
 //greeting and goodbye buttons
-function greeting() {
+function hello() {
     alert("Hello and Welcome :D");
 }
 
@@ -30,7 +30,7 @@ function question4() {
 
 //math test thing
 function varDemo() {
-    alert("check the console and do some math..addition!")
+    alert("check the console and do some math..addition!");
     let firstnumber = 2;
     let secondnumber = 3;
     let answer = firstnumber + secondnumber;
@@ -38,31 +38,31 @@ function varDemo() {
 }
 
 function varDemo2() {
-    alert("check the console and do some math..but division this time!")
-    let firstnumber = 45
-    let secondnumber = 5
+    alert("check the console and do some math..but division this time!");
+    let firstnumber = 45;
+    let secondnumber = 5;
     let answer = firstnumber / secondnumber
     console.log(answer + " ...numbers are fun :)")
 }
 
 function varDemo3() {
-    alert("check the console and do some math..we're ending with multiplication!")
-    let firstnumber = 6
-    let secondnumber = 7
-    let answer = firstnumber * secondnumber
-    console.log(answer + " ...woo math!")
+    alert("check the console and do some math..we're ending with multiplication!");
+    let firstnumber = 6;
+    let secondnumber = 7;
+    let answer = firstnumber * secondnumber;
+    console.log(answer + " ...woo math!");
 }
 
 //random button
 function midway() {
-    alert("yay! You're halfway through the list of buttons!")
+    alert("yay! You're halfway through the list of buttons!");
 }
 
 //review
 function review(yes, no){
-    let rating = prompt("Did you like this page? Yes or no?")
+    let rating = prompt("Did you like this page? Yes or no?");
     if (rating === "yes"){
-    alert("yay! I'm so so glad you liked it! :)") ; 
+    alert("yay! I'm so so glad you liked it! :)"); 
 }
     else if (rating === "no"){
         alert("oh no! I'll be sure to do better next time!");
@@ -71,15 +71,15 @@ function review(yes, no){
         alert("ah, I see");
 }
 }
-// Prompts the user for a integer and returns a message on if its odd or even
+//Prompts the user for a integer and returns a message on if its odd or even
 //this is a test
 function oddOrEven() {
-    let userNum = prompt("Enter an integer")
+    let userNum = prompt("Enter an integer");
     if (userNum %2 == 1) {
-            alert("That number is odd")
+            alert("That number is odd");
     }
     else {
-        alert("That number is even")
+        alert("That number is even");
     }
 }
 
@@ -98,14 +98,31 @@ function whileSumThrees() {
 //Create a JavaScript function, largerNumber() that prompts the 
 //user for two integers and uses conditional statement(s) to alert the user 
 //to the larger of the two numbers.
-function largerNumber() {
-    let number = prompt ("Enter 2 integers")
-    let number = 0
-    let num2 = 0
-    if (number < num2) {
-        alert (num2 + " is greater")
+
+
+//Create a JavaScript function, signOfProduct(), that prompts the user 
+//for 3 integers and uses conditional statement(s) to output the sign of 
+//product of three numbers to the user in an alert. 
+
+//Create a JavaScript function, forSumThrees(), that uses a for loop to sum the 
+//multiples of 3 under 1000. Log the result to the console
+function forSumThrees() {
+    let sum = 0
+    for (let i = 0; i < 1000; i = i + 3){
     }
-    else {
-        alert (number + " is greater")
+    console.log(sum);
+}
+
+//Create a JavaScript function, centuryFromYear(), that,
+// given a year, returns the century it is in. The first century 
+//spans from the year 1 up to and including the year 100, The second - 
+//from the year 101 up to and including the year 200, etc.
+function centuryFromYear(){
+    let year = prompt ("Enter a year")
+    if (year < 101){
+        alert("that is the first century")
+    }
+    else if (year < 201){
+        alert("that is the 2nd century")
     }
 }
